5ce43db6e11914a4012e9048ba77ed6c6e6323d9
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-19 10:39:10 +0530

    next student django project

---
+++ stud/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import View
+from stud.models import StudentModel
+
+# Create your views here.
+
+class studentCreateView(View):
+
+    def get(self,request):
+
+        return render(request,"create_stud.html")
+    
+    def post(self,request):
+
+        print(request.POST)
+
+        StudentModel.objects.create(name = request.POST.get("name"),
+                                     roll_no =request.POST.get("rollno"),
+                                      department=request.POST.get("dept"),
+                                       email =request.POST.get("email"),
+                                       marks =request.POST.get("mark")
+                                         )
+
+        return render(request,"create_stud.html")
+
+
+
+
